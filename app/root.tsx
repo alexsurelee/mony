80af8fc391299a5a5c372a993733453f9dafef97
Alexander Lee <alex@surelee.dev>
2022-04-15 18:21:57 +1200

    feat(style): add mantine
    I should probably avoid reinventing the wheel and instead focus on making business logic a bit
    sooner

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,10 @@
 import {
+  Link,
   Links,
   LiveReload,
   LoaderFunction,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -11,6 +13,13 @@ import type { MetaFunction } from "remix";
 import type { LinksFunction } from "remix";
 import { ClerkApp, ClerkCatchBoundary } from "@clerk/remix";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import {
+  MantineProvider,
+  AppShell,
+  Navbar,
+  Header,
+  Anchor,
+} from "@mantine/core";
 
 import stylesUrl from "~/root.css";
 import NavBar, { links as navBarLinks } from "./components/NavBar/NavBar";
@@ -30,21 +39,38 @@ export const CatchBoundary = ClerkCatchBoundary();
 
 function App() {
   return (
-    <html lang="en-NZ">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <NavBar />
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <MantineProvider withCSSVariables withNormalizeCSS withGlobalStyles>
+      <html lang="en-NZ">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <AppShell
+            header={
+              <Header height={60} p="xs">
+                <Anchor component={Link} to="/">
+                  KiwiBudget
+                </Anchor>
+                <Anchor component={NavLink} to="/budget">
+                  Budget
+                </Anchor>
+                <Anchor component={NavLink} to="/accounts">
+                  Accounts
+                </Anchor>
+              </Header>
+            }
+          >
+            <Outlet />
+          </AppShell>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    </MantineProvider>
   );
 }
 
