779dce68878f169fcc818cbb602e2771b1b3ca8e
Alexander Lee <alexanderlee@canva.com>
2025-02-01 21:46:36 +1300

    fixups

--- remix.config.js
+++ remix.config.js
@@ -3,11 +3,9 @@
  */
 module.exports = {
   serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
 };
